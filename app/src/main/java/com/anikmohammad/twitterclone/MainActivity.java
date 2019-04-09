c4bb24dddd6f1a46ba407711fd9c2d417f745549
Anik Mohammad <anik7303@gmail.com>
2019-04-09 20:38:48 +0600

    starting fresh

--- app/src/main/java/com/anikmohammad/twitterclone/MainActivity.java
+++ app/src/main/java/com/anikmohammad/twitterclone/MainActivity.java
@@ -1,16 +1,8 @@
 package com.anikmohammad.twitterclone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-import com.parse.LogInCallback;
-import com.parse.LogOutCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,57 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        // sign up a user named 'root'
-//        ParseUser user = new ParseUser();
-//        user.setUsername("root");
-//        user.setPassword("root");
-//        user.signUpInBackground(new SignUpCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if(e == null) {
-//                    Toast.makeText(MainActivity.this, "root successfully signed up", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    handleException(e, "ParseUser SignUp");
-//                }
-//            }
-//        });
-    }
-
-    protected void testServer(View view) {
-        if(ParseUser.getCurrentUser() == null) {
-            final String username = "root";
-            final String password = "root";
-            ParseUser.logInInBackground(username, password, new LogInCallback() {
-                @Override
-                public void done(ParseUser user, ParseException e) {
-                    if(e == null && user != null) {
-                        Toast.makeText(MainActivity.this, String.format("%s successfully logged in", username), Toast.LENGTH_SHORT).show();
-                    } else if(e == null) {
-                        Toast.makeText(MainActivity.this, "user == null", Toast.LENGTH_SHORT).show();
-                    } else {
-                        handleException(e, "ParseUser Login");
-                    }
-                }
-            });
-        } else {
-            Toast.makeText(MainActivity.this, String.format("username: %s", ParseUser.getCurrentUser().getUsername()), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    protected void logout(View view) {
-        if(ParseUser.getCurrentUser() != null) {
-            ParseUser.logOutInBackground(new LogOutCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if(e == null) {
-                        Toast.makeText(MainActivity.this, "Logout successful", Toast.LENGTH_SHORT).show();
-                    } else {
-                        handleException(e, "ParseUser Logout");
-                    }
-                }
-            });
-        }
     }
 
     private void handleException(Exception exception, String title) {
