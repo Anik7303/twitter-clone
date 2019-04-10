b15f23885999a04acbdf5a171c91f466f65fa4f0
Anik Mohammad <anik7303@gmail.com>
2019-04-10 18:30:07 +0600

    added sent tweet functionality

--- app/src/main/java/com/anikmohammad/twitterclone/UserFeedActivity.java
+++ app/src/main/java/com/anikmohammad/twitterclone/UserFeedActivity.java
@@ -1,14 +1,22 @@
 package com.anikmohammad.twitterclone;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class UserFeedActivity extends AppCompatActivity {
 
@@ -32,6 +40,34 @@ public class UserFeedActivity extends AppCompatActivity {
         Intent intent;
         switch(item.getItemId()) {
             case R.id.tweet:
+                final EditText tweetEditText = new EditText(UserFeedActivity.this);
+                new AlertDialog.Builder(UserFeedActivity.this)
+                        .setTitle("Send a tweet")
+                        .setView(tweetEditText)
+                        .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ParseObject tweetObject = new ParseObject("Tweets");
+                                tweetObject.put("usename", ParseUser.getCurrentUser().getUsername());
+                                tweetObject.put("tweet", tweetEditText.getText().toString());
+                                tweetObject.saveInBackground(new SaveCallback() {
+                                    @Override
+                                    public void done(ParseException e) {
+                                        if(e == null) {
+                                            Toast.makeText(UserFeedActivity.this, "Tweet sent", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();
+
                 break;
             case R.id.user_feed:
                 intent = new Intent(UserFeedActivity.this, TweetsActivity.class);
